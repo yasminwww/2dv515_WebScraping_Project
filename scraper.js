52a34e8fea1ba855add0a8c369da0a459d225a8a
Yasmin <ym222bs@student.lnu.se>
2020-01-13 22:25:13 +0100

    scraping 200 pages and writing to file

--- scraper.js
+++ scraper.js
@@ -1,20 +1,70 @@
+'use strict'
+
 const cheerio = require('cheerio')
-const request = require('request')
+const rp = require('request-promise')
+const fs = require('fs').promises
+
+const initialURL = 'https://en.wikipedia.org/wiki/Art'
+const testURL = 'https://en.wikipedia.org'
 
 
-const getHTML = () => {
-    const siteLinks = []
-    request('https://en.wikipedia.org/wiki/Art', (err, res, html) => {
-        if(err) console.log('Error:: ', err.message)
-        if(res.statusCode == 200) {
-            const $ = cheerio.load(html)
 
-            const content = $('#content')
-            console.log(content.html())
-            // console.log(content.text())
+const getRawHTML = async () => {
+    const options = {
+            uri: initialURL,
+            transform: (body) => {
+            return cheerio.load(body)
         }
-    })
-
+    }
+    try {
+        const $ = await rp(options)
+        extractLinks($)
+        // scrapeRawHTMLFromEachLink($)
+        return $
+    }
+    catch (err) {
+        console.log('getRawHTML', err.message)
+    }
 }
 
-getHTML()
+
+const extractLinks = ($) => {
+    const setOfLinks = new Set()
+    const content = $('#mw-content-text')
+    const linksFoundOnPage = content.find('a')
+    $(linksFoundOnPage).each((i, element) => {
+        if(setOfLinks.size <= 200) {
+            let link = $(element).attr('href')
+            if(link !== undefined && link.includes('/wiki/') && !link.includes(':')) {
+                setOfLinks.add(link) 
+            }
+        }
+    })
+    scrapeRawHTMLFromEachLink(setOfLinks)
+    return setOfLinks
+}
+
+
+// FUN: Randomize 200 ?
+const scrapeRawHTMLFromEachLink = async (setOfLinks) => {
+    let htmls = ''
+    let count = 0
+    setOfLinks.forEach(async (fragment) => {
+        try {
+            const options = {
+                uri: `${testURL}${fragment}`,
+                transform: (body) => {
+                    return cheerio.load(body)
+                }
+            }
+            const $ = await rp(options)
+            htmls = $('#mw-content-text').html()
+        } catch (err) {
+            console.log('ScrapeRawHTMLFROMEACH', err.message)
+        }
+        await fs.writeFile(`rawHTML${count++}.html`, htmls)
+
+    })
+}
+
+getRawHTML()
